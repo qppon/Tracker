960b8fbc58f059db01596ef7874272e597446932
qppon <piter7638@gmail.com>
2025-03-07 14:30:31 +0300

    Начало проекта

---
+++ MainScreen/TabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  TabBarController.swift
+//  Tracker
+//
+//  Created by Jojo Smith on 3/7/25.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    super.awakeFromNib() {
+        
+    }
+}
---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Jojo Smith on 3/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
